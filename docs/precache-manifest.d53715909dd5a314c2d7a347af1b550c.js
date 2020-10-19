17dc2ca1ccf97620aafc11896261387e48784209
Jim Hopkinson <jahopki@hotmail.com>
2020-10-19 05:13:12 -0600

    add docs?

---
+++ docs/precache-manifest.d53715909dd5a314c2d7a347af1b550c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06fcdbae1d89427344bdb44f5542e42",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "bc06893c73aaf2803de0",
+    "url": "/React-Portfolio/static/css/main.4f7446cc.chunk.css"
+  },
+  {
+    "revision": "4cc9417ab9eb5441b45e",
+    "url": "/React-Portfolio/static/js/2.2ca4e47f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.2ca4e47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc06893c73aaf2803de0",
+    "url": "/React-Portfolio/static/js/main.7647237b.chunk.js"
+  },
+  {
+    "revision": "ed6bc7f7c45ee59528aa",
+    "url": "/React-Portfolio/static/js/runtime-main.ebea63a7.js"
+  },
+  {
+    "revision": "7df18a3bc6737c14549d385445111448",
+    "url": "/React-Portfolio/static/media/SampleFunctionalResume.7df18a3b.pdf"
+  }
+]);
